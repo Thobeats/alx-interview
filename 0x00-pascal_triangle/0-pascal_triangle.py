27fde8b095709a08654732728752769048b2e2f1
Iyanu Dayisi <tobiy23@gmail.com>
2024-07-28 22:46:38 +0100

    write pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """A function that creates a pacal triangle"""
+
+    triangle = list()
+    i = 1
+    if n <= 0:
+        return triangle
+    
+    while i <= n:
+        if i == 1:
+            triangle.append([1])
+        if i == 2:
+            triangle.append([1,1])
+        if i > 2:
+            len_of_prev_result = len(triangle[i - 2])
+            j = 0
+            new_list = list()
+            new_list.append(1)
+            while j < len_of_prev_result - 1:
+                new_list.append(triangle[i - 2][j] + triangle[i - 2][j + 1])
+                j += 1
+            new_list.append(1)
+            triangle.append(new_list)
+        i += 1
+    return triangle
