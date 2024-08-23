6b580ebe121437225405cc9d74fdf716f589b1b2
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-23 02:25:47 +0100

    validate utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    """ValidUTF8
+
+    Keyword arguments:
+    data -- validates the utf8
+    Return: True or False
+    """
+
+    bytes_remaining = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if bytes_remaining == 0:
+            if byte >> 5 == 0b110:
+                bytes_remaining = 1
+            elif byte >> 4 == 0b1110:
+                bytes_remaining = 2
+            elif byte >> 3 == 0b11110:
+                bytes_remaining = 3
+            elif byte >> 7:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            bytes_remaining -= 1
+
+    return bytes_remaining == 0
