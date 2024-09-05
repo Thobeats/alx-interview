b2c9c99065a2e8f8c41f677f66139a12961b2d57
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-06 00:27:26 +0100

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,15 +7,19 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach(element => {
-      request(element, function (error, response, body) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   }
 });
