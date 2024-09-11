9f911b8f95535b28f4c66b445c20422ddf3f877c
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-11 21:50:18 +0100

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 
 
 def rotate_2d_matrix(matrix: list[list]):
+    """Rotate 2D Matrix in 90 degrees"""
     matrix[:] = [
         [matrix[len(matrix) - j - 1][i] for j in range(len(matrix))]
         for i in range(len(matrix[0]))
